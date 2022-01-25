1b679b473716bf201bf5d0f64e055f08a259d20b
Alvaro Tinoco <alvarotinocomarmol@gmail.com>
2022-01-25 19:21:31 +0100

    fix: some layouts were out of image

--- internal/zmk-layout-viewer/layout-viewer.go
+++ internal/zmk-layout-viewer/layout-viewer.go
@@ -13,35 +13,22 @@ import (
 	"golang.org/x/image/font/gofont/goregular"
 )
 
-func Generate(board string, file string, trasnparent bool, output string) error {
+const (
+	keySize  = 40.0
+	spacer   = 5.0
+	fontSize = 10.0
+)
 
+func Generate(board string, file string, trasnparent bool, output string) error {
 	l, err := keyboard.Fetch(board)
 	if err != nil {
 		return err
 	}
 
 	if file != "" {
-		log.Info().Msg("Parsing keymap file.")
-		log.Debug().Str("board", board).Str("file", file).Send()
-		r, err := os.Open(file)
+		err = withKeymap(file, output, trasnparent, l)
 		if err != nil {
 			return err
-		}
-
-		ast, err := keymap.Parse(r)
-		defer r.Close()
-
-		if err != nil {
-			return err
-		}
-
-		for _, layer := range ast.Sections[len(ast.Sections)-1].Device.Keymap.Layers {
-			for _, layout := range l {
-				err = drawLayer(layer, &layout, output, trasnparent)
-				if err != nil {
-					return err
-				}
-			}
 		}
 	} else {
 		for _, layout := range l {
@@ -55,16 +42,45 @@ func Generate(board string, file string, trasnparent bool, output string) error 
 	return nil
 }
 
-func drawLayer(layer *keymap.Layer, layout *keyboard.Layout, output string, transparent bool) error {
+func withKeymap(file string, output string, trasnparent bool, l map[string]keyboard.Layout) error {
+	log.Info().Msg("Parsing keymap file.")
+	r, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+
+	ast, err := keymap.Parse(r)
+	defer r.Close()
+
+	if err != nil {
+		return err
+	}
+
+	for _, layer := range ast.Sections[len(ast.Sections)-1].Device.Keymap.Layers {
+		for _, layout := range l {
+			err = drawLayer(layer, &layout, output, trasnparent)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func createContext(layout *keyboard.Layout) *gg.Context {
 	mx := maxX(layout.Layout) + 1
 	my := maxY(layout.Layout) + 1
 
-	imageW := (mx * 40) + (mx+1)*5
-	imageH := int(math.Ceil((my*40.)+(my+1.)*5.) + 20)
+	imageW := int((mx*keySize)+(mx*spacer)) + spacer
+	imageH := int(math.Ceil((my*keySize)+(my+1.)*spacer) + (fontSize + spacer*2))
 
 	log.Debug().Int("Image Width", imageW).Int("Image Height", imageH).Send()
 
-	dc := gg.NewContext(imageW, imageH)
+	return gg.NewContext(int(imageW), imageH)
+}
+
+func drawLayer(layer *keymap.Layer, layout *keyboard.Layout, output string, transparent bool) error {
+	dc := createContext(layout)
 	font, err := truetype.Parse(goregular.TTF)
 	if err != nil {
 		return err
@@ -84,22 +100,26 @@ func drawLayer(layer *keymap.Layer, layout *keyboard.Layout, output string, tran
 
 	if layer != nil {
 		dc.SetRGB(0., 0., 0.)
-		dc.DrawString(layer.Name, 5., 15.)
+		dc.DrawString(layer.Name, spacer, fontSize+spacer)
 	}
 
 	for i, key := range layout.Layout {
-		w := 40.0
-		x := key.X*w + 5.0*key.X + 5
-		y := key.Y*w + 5.0*key.Y + 20
+		x := key.X*keySize + spacer*key.X + spacer
+		y := key.Y*keySize + spacer*key.Y + (fontSize + spacer*2)
 
-		dc.DrawRoundedRectangle(x, y, key.W*w, w, 5.)
+		if key.H != nil {
+			dc.DrawRoundedRectangle(x, y, key.W*keySize, *key.H*keySize, spacer)
+		} else {
+			dc.DrawRoundedRectangle(x, y, key.W*keySize, keySize, spacer)
+		}
+
 		dc.SetRGB(0., 0., 0.)
 		dc.StrokePreserve()
 		dc.SetRGB(1.0, 1.0, 1.0)
 		dc.Fill()
 
 		if layer != nil {
-			drawBehavior(dc, layer.Bindings[i], x+w/2., y+w/2.)
+			drawBehavior(dc, layer.Bindings[i], x+keySize/2., y+keySize/2.)
 		}
 	}
 	if layer != nil {
@@ -136,16 +156,17 @@ func drawBehavior(ctx *gg.Context, key *keymap.Behavior, x float64, y float64) {
 	}
 }
 
-func maxX(l []keyboard.Key) int {
-	curr := 0
+func maxX(l []keyboard.Key) float64 {
+	curr := 0.
 	for _, v := range l {
-		if v.X > float64(curr) {
-			curr = int(v.X)
+		if v.X > curr {
+			curr = v.X
 		}
 	}
-	log.Debug().Int("Max X", curr).Send()
+	log.Debug().Float64("Max X", curr).Send()
 	return curr
 }
+
 func maxY(l []keyboard.Key) float64 {
 	curr := 0.
 	for _, v := range l {
